b1d43223ab5946ac491ff924f3d74f8de6ec4ee6
Piotr Zakrzewski <piotr@thehyve.nl>
2016-04-11 15:09:01 +0200

    Fix missing Controller annotation. Move configs to the top-level so that component scan works properly.

--- src/main/java/nl/thehyve/ocdu/controllers/DataUploadController.java
+++ src/main/java/nl/thehyve/ocdu/controllers/DataUploadController.java
@@ -1,15 +1,19 @@
 package nl.thehyve.ocdu.controllers;
 
+import nl.thehyve.ocdu.services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by piotrzakrzewski on 11/04/16.
  */
+@Controller
 public class DataUploadController {
 
     @RequestMapping(value="/data", method= RequestMethod.GET)
-    public String login() {
+    public String data() {
         return "data";
     }
 
--- src/main/java/nl/thehyve/ocdu/controllers/EventsRegistrationController.java
+++ src/main/java/nl/thehyve/ocdu/controllers/EventsRegistrationController.java
@@ -1,15 +1,17 @@
 package nl.thehyve.ocdu.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by piotrzakrzewski on 11/04/16.
  */
+@Controller
 public class EventsRegistrationController {
 
     @RequestMapping(value="/events", method= RequestMethod.GET)
-    public String login() {
+    public String events() {
         return "events";
     }
 

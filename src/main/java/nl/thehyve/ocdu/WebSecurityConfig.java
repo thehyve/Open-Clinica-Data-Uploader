1f970e0270c10d7a411e4013dc1e8b46c5ba48cb
bo <beaugogh@users.noreply.github.com>
2016-04-13 14:13:11 +0200

    Login (#7)
    * add notes
    
    * add notes
    
    * login update
    
    * login templates update
    
    * header change
    
    * login upate
    
    * login update
    
    * login update
    
    * move style.css for better structuring the project
    
    modify html templates to include login header bar
    
    modify header.html template
    
    login upate
    
    add testing notes, which should be removed later
    
    modify WebSecurityConfig with default SpringSecurity auth mapping
    
    * add bootstrap js files for styling, which are included in the footer template
    
    * add notes
    
    * move style.css for better structuring the project
    
    modify html templates to include login header bar
    
    modify header.html template
    
    login upate
    
    add testing notes, which should be removed later
    
    modify WebSecurityConfig with default SpringSecurity auth mapping
    
    * add bootstrap js files for styling, which are included in the footer template
    
    * clean up the files, incl. delete unnecessary ones and modify existing ones

--- src/main/java/nl/thehyve/ocdu/WebSecurityConfig.java
+++ src/main/java/nl/thehyve/ocdu/WebSecurityConfig.java
@@ -6,12 +6,16 @@ package nl.thehyve.ocdu;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
 
 @Configuration
 @EnableWebSecurity
@@ -19,14 +23,15 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/index").authenticated()
+            .authorizeRequests()
+                .antMatchers("/", "/data").authenticated()
                 .and()
-                .formLogin()
+            .formLogin()
                 .loginPage("/login")
+//                .successHandler(successHandler())
                 .permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+            .logout()
                 .permitAll();
     }
 
@@ -36,4 +41,13 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
+//    @Bean
+//    public AuthenticationSuccessHandler successHandler() {
+//        SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+//        handler.setUseReferer(true);
+//        handler.setDefaultTargetUrl("/events");
+//        return handler;
+//    }
+
 }
--- src/main/java/nl/thehyve/ocdu/controllers/DataUploadController.java
+++ src/main/java/nl/thehyve/ocdu/controllers/DataUploadController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DataUploadController {
 
-    @RequestMapping(value="/data", method= RequestMethod.GET)
+    @RequestMapping(value="/", method= RequestMethod.GET)
     public String data() {
         return "data";
     }

88aa9ce7055097a4e95f4381014bacc2cf294a89
Piotr Zakrzewski <piotr@thehyve.nl>
2016-05-10 16:37:03 +0200

    Implement CrfCouldNotBeVerifiedError Cross Checking for clinical data. Refactor ClinicalDataCrossCheck interface.

--- src/main/java/nl/thehyve/ocdu/validators/crossChecks/ClinicalDataCrossCheck.java
+++ src/main/java/nl/thehyve/ocdu/validators/crossChecks/ClinicalDataCrossCheck.java
@@ -1,10 +1,14 @@
 package nl.thehyve.ocdu.validators.crossChecks;
 
+import nl.thehyve.ocdu.models.OcDefinitions.CRFDefinition;
 import nl.thehyve.ocdu.models.OcDefinitions.MetaData;
 import nl.thehyve.ocdu.models.OCEntities.ClinicalData;
 import nl.thehyve.ocdu.models.errors.ValidationErrorMessage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by piotrzakrzewski on 04/05/16.
@@ -12,5 +16,17 @@ import java.util.List;
 public interface ClinicalDataCrossCheck {
 
     ValidationErrorMessage getCorrespondingError(List<ClinicalData> data, MetaData metaData);
-
+     default Map<String, List<String>> buildEventMap(MetaData metaData) {
+        Map<String, List<String>> eventMap = new HashMap<>();
+        metaData.getEventDefinitions().stream().forEach(eventDefinition ->
+                {
+                    List<String> crfNames = new ArrayList<String>();
+                    for (CRFDefinition crf : eventDefinition.getCrfDefinitions()) {
+                        crfNames.add(crf.getOid());
+                    }
+                    eventMap.put(eventDefinition.getStudyEventOID(), crfNames);
+                }
+        );
+        return eventMap;
+    }
 }
--- src/main/java/nl/thehyve/ocdu/validators/crossChecks/CrfCouldNotBeVerifiedCrossCheck.java
+++ src/main/java/nl/thehyve/ocdu/validators/crossChecks/CrfCouldNotBeVerifiedCrossCheck.java
@@ -1,30 +1,28 @@
 package nl.thehyve.ocdu.validators.crossChecks;
 
-import nl.thehyve.ocdu.models.OcDefinitions.MetaData;
 import nl.thehyve.ocdu.models.OCEntities.ClinicalData;
-import nl.thehyve.ocdu.models.OcDefinitions.CRFDefinition;
+import nl.thehyve.ocdu.models.OcDefinitions.MetaData;
 import nl.thehyve.ocdu.models.errors.CRFDoesNotExist;
+import nl.thehyve.ocdu.models.errors.CrfCouldNotBeVerified;
 import nl.thehyve.ocdu.models.errors.ValidationErrorMessage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * Created by piotrzakrzewski on 04/05/16.
+ * Created by piotrzakrzewski on 10/05/16.
  */
-public class CrfExistsCrossCheck implements ClinicalDataCrossCheck {
+public class CrfCouldNotBeVerifiedCrossCheck implements ClinicalDataCrossCheck {
     @Override
     public ValidationErrorMessage getCorrespondingError(List<ClinicalData> data, MetaData metaData) {
         Map<String, List<String>> eventMap = buildEventMap(metaData);
-        List<ClinicalData> crfNotExistsOffenders = getCrfNotExistsOffenders(data, eventMap);
-
-        if (crfNotExistsOffenders.size() > 0) {
-            CRFDoesNotExist error = new CRFDoesNotExist("One or more CRFs you used in your data file is not present in the referenced event");
+        List<ClinicalData> crfCouldNotBeVerifiedOffenders = getcrfCouldNotBeVerifiedOffenders(data, eventMap);
+        if (crfCouldNotBeVerifiedOffenders.size() > 0) {
+            CrfCouldNotBeVerified error = new CrfCouldNotBeVerified();
             List<String> offendingNames = new ArrayList<>();
-            crfNotExistsOffenders.stream().forEach(clinicalData -> {
+            crfCouldNotBeVerifiedOffenders.stream().forEach(clinicalData -> {
                 String crf = clinicalData.getCrfName();
                 if (!offendingNames.contains(crf)) offendingNames.add(crf);
             });
@@ -33,18 +31,12 @@ public class CrfExistsCrossCheck implements ClinicalDataCrossCheck {
         } else return null;
     }
 
-
-
-    private List<ClinicalData> getCrfNotExistsOffenders(List<ClinicalData> data, Map<String, List<String>> eventMap) {
+    private List<ClinicalData> getcrfCouldNotBeVerifiedOffenders(List<ClinicalData> data, Map<String, List<String>> eventMap) {
         return data.stream().filter(clinicalData -> {
             List<String> valid = eventMap.get(clinicalData.getEventName());
-            if (valid == null) return false; // CRF Could not be verified is a separate class
-            String crf = clinicalData.getCrfName();
-            if (!valid.contains(crf))
-                return true;
+            if (valid == null) return true;
             else return false;
         }).collect(Collectors.toList());
     }
-
 
 }

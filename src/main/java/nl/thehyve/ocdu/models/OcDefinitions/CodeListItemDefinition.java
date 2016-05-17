5417bf79a655a2f4a7c3e2a633d64cf82e9c31b5
Piotr Zakrzewski <piotr@thehyve.nl>
2016-05-17 14:21:13 +0200

    Implement parsing codelists and multipleselect lists.

--- src/main/java/nl/thehyve/ocdu/models/OcDefinitions/CodeListItemDefinition.java
+++ src/main/java/nl/thehyve/ocdu/models/OcDefinitions/CodeListItemDefinition.java
@@ -16,11 +16,21 @@ public class CodeListItemDefinition  {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    private String content;
+
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }

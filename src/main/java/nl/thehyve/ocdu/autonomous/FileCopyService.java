34828359f996c652111dfc418f4ba5fdbfebbeb6
Jacob Rousseau <j.rousseau@nki.nl>
2016-05-24 11:35:11 +0200

    Addtion of the stub of the FileCopyService

---
+++ src/main/java/nl/thehyve/ocdu/autonomous/FileCopyService.java
@@ -0,0 +1,21 @@
+package nl.thehyve.ocdu.autonomous;
+
+/**
+ * Class which is responsible for file traffic for the autonomous upload
+ * Created by jacob on 5/24/16.
+ */
+public class FileCopyService {
+
+    private String sourceDirectory;
+
+    private String openClinicaPickupDirectory;
+
+    private String failedFilesDirectory;
+
+    private String finishedFilesDirectory;
+
+    public FileCopyService() {
+
+    }
+    
+}

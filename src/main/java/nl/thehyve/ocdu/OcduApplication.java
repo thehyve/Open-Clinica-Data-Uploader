d9c0014f24a5f0c96fddefa5bab303b7ebfe4a88
Jacob Rousseau <j.rousseau@nki.nl>
2016-05-24 08:52:39 +0200

    Ongoing work on the autonomous upload: finished the automatic scheduling

--- src/main/java/nl/thehyve/ocdu/OcduApplication.java
+++ src/main/java/nl/thehyve/ocdu/OcduApplication.java
@@ -8,12 +8,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Date;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableScheduling
 public class OcduApplication {
 
     public static void main(String[] args) {
@@ -24,6 +30,9 @@ public class OcduApplication {
     /*@Bean
     public CommandLineRunner testData(UploadSessionRepository repository, OCUserRepository usrRepository) {
         return (args) -> {
+            log.info("Loading autonomous module...");
+            ApplicationContext context =
+                    new ClassPathXmlApplicationContext(new String[] {"autonomous.xml"});
             log.info("Generating test data ...");
             OcUser bogusUser= new OcUser();
             bogusUser.setUsername("bogao");

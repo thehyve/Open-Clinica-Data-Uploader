3becf1878ef491be345d178b90b0f5310a6b7e2e
Jacob Rousseau <j.rousseau@nki.nl>
2016-05-25 15:54:34 +0200

    Ongoing work. Removed the workdirectory and addtion of a timestamp to the file name when copying it to the 'completed' directory

--- src/main/java/nl/thehyve/ocdu/autonomous/FileCopyService.java
+++ src/main/java/nl/thehyve/ocdu/autonomous/FileCopyService.java
@@ -1,6 +1,8 @@
 package nl.thehyve.ocdu.autonomous;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -8,9 +10,10 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -28,30 +31,26 @@ public class FileCopyService {
 
     private String completedFilesDirectory;
 
-    private String workDirectory;
 
     public FileCopyService() throws Exception {
-    }
-
-    public void start() throws Exception {
         log.info("Starting FileCopyService");
         log.info("");
         log.info("Source directory: " + sourceDirectory);
         log.info("Failed files directory: " + failedFilesDirectory);
         log.info("Completed files directory: " + completedFilesDirectory);
-        log.info("Work directory: " + workDirectory);
         log.info("");
+    }
+
+    public void start() throws Exception {
         checkReadWriteAccess();
-        FileUtils.copyDirectory(new File(sourceDirectory), new File(workDirectory));
-        log.info("Copied files to work directory");
     }
 
 
-    public List<Path> obtainWorkPathList() {
+    public List<Path> obtainWorkList() {
         // explicit naming of variables for clarity of the call to FileUtils.listFiles.
         boolean recursive = false;
         String[] extensionList = null;
-        Collection<File> fileCollection = FileUtils.listFiles(new File(workDirectory), extensionList, recursive);
+        Collection<File> fileCollection = FileUtils.listFiles(new File(sourceDirectory), extensionList, recursive);
         ArrayList<Path> ret = new ArrayList<Path>();
         for (File file : fileCollection) {
             ret.add(file.toPath());
@@ -60,18 +59,24 @@ public class FileCopyService {
     }
 
     public void stop() throws Exception {
-        FileUtils.copyDirectory(new File(workDirectory), new File(failedFilesDirectory));
-        log.info("Aborted and copyied all files to the failed files direcotry");
     }
 
     public void failedFile(Path path) throws Exception {
-        Files.copy(path, new File(failedFilesDirectory).toPath(), StandardCopyOption.ATOMIC_MOVE);
+        String newFileName = addTimeStamp(path, failedFilesDirectory);
+        FileUtils.moveFile(path.toFile(), new File(newFileName));
         log.info("Failed file '" + path + "' moved to failed files directory");
     }
 
     public void successfulFile(Path path) throws Exception {
-        Files.copy(path, new File(completedFilesDirectory).toPath(), StandardCopyOption.ATOMIC_MOVE);
-        log.info("Sucessfully completed file '" + path + "' moved to completed files directory");
+        String newFileName = addTimeStamp(path, completedFilesDirectory);
+        FileUtils.moveFile(path.toFile(), new File(newFileName));
+        log.info("Successfully completed file '" + path + "' moved to completed files directory");
+    }
+
+    private static String addTimeStamp(Path aPath, String aDestinationDirectory) {
+        Path fileName = aPath.getFileName();
+        String newFileName = DateFormatUtils.format(GregorianCalendar.getInstance(), "yyyyMMddHHmmssSSS") + "-" + fileName.toString();
+        return aDestinationDirectory + File.separator + newFileName;
     }
 
     public void setSourceDirectory(String sourceDirectory) {
@@ -86,9 +91,7 @@ public class FileCopyService {
         this.completedFilesDirectory = completedFilesDirectory;
     }
 
-    public void setWorkDirectory(String workDirectory) {
-        this.workDirectory = workDirectory;
-    }
+
 
 
     private void checkReadWriteAccess() throws Exception {
@@ -100,13 +103,6 @@ public class FileCopyService {
         }
         if (! Files.isWritable(new File(completedFilesDirectory).toPath())) {
             throw new IllegalStateException("Completed files directory '" + completedFilesDirectory + "' is not writable for application");
-        }
-
-        if (! Files.isReadable(new File(workDirectory).toPath())) {
-            throw new IllegalStateException("Work directory '" + workDirectory + "' is not readable for application");
-        }
-        if (! Files.isWritable(new File(workDirectory).toPath())) {
-            throw new IllegalStateException("Work directory '" + workDirectory + "' is not writable for application");
         }
     }
 }

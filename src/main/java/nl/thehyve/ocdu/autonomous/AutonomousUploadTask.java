9583c88fd3d5aa3ef9ce61083ba3dd4b89a4b292
Jacob Rousseau <j.rousseau@nki.nl>
2016-05-25 15:54:34 +0200

    Ongoing work. Removed the workdirectory and addtion of a timestamp to the file name when copying it to the 'completed' directory

--- src/main/java/nl/thehyve/ocdu/autonomous/AutonomousUploadTask.java
+++ src/main/java/nl/thehyve/ocdu/autonomous/AutonomousUploadTask.java
@@ -2,8 +2,6 @@ package nl.thehyve.ocdu.autonomous;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
@@ -25,10 +23,16 @@ public class AutonomousUploadTask {
         log.info("Running autonomousUploadTask...");
         try {
             fileCopyService.start();
-            List<Path> filePathList = fileCopyService.obtainWorkPathList();
-            for (Path filePath : filePathList) {
-                // how do you obtain the associated mapping file from the input files?
-                fileCopyService.successfulFile(filePath);
+            List<Path> filePathList = fileCopyService.obtainWorkList();
+            if (filePathList.size() > 0) {
+                log.info("Found " + filePathList.size() + " files.");
+                for (Path filePath : filePathList) {
+                    // how do you obtain the associated mapping file from the input files?
+                    fileCopyService.successfulFile(filePath);
+                }
+            }
+            else {
+                log.info("No files found, stopping.");
             }
             fileCopyService.stop();
         }

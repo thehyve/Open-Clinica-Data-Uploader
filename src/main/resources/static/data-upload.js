7c63a8cae350d3fe3a882a06eb7f36c09a958cdb
Bo Gao <bo.gao111@gmail.com>
2016-06-06 10:16:26 +0200

    remove data-upload success message and directly redirect user to the mapping view

--- src/main/resources/static/data-upload.js
+++ src/main/resources/static/data-upload.js
@@ -80,8 +80,6 @@ function uploadFile() {
             success: function (fileFormatErrors) {
                 if (fileFormatErrors.length === 0) {
                     // Handle upload success
-                    var info = '<span id="data-alert" class="alert alert-success">Data successfully read.</span>';
-                    $("#message-board").append(info);
                     mappingFileUpload();
 
                 } else {

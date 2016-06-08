193a4d22849f8fb8a20e66aa03be8ded814d3716
Ruslan Forostianov <ruslan@thehyve.nl>
2016-06-08 14:57:42 +0200

    Remove unnecessary annotation.

--- src/main/java/nl/thehyve/ocdu/OcduApplication.java
+++ src/main/java/nl/thehyve/ocdu/OcduApplication.java
@@ -1,24 +1,12 @@
 package nl.thehyve.ocdu;
 
-import nl.thehyve.ocdu.models.OcUser;
-import nl.thehyve.ocdu.models.UploadSession;
-import nl.thehyve.ocdu.repositories.UploadSessionRepository;
-import nl.thehyve.ocdu.repositories.OCUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Date;
-
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableScheduling
 public class OcduApplication {
 

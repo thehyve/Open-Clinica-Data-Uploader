92544739035e84b650ad7cbb93d73eed69dcd875
Piotr Zakrzewski <piotr@thehyve.nl>
2016-06-17 16:27:39 +0200

    Implement creating requests for subjectRegistration

--- src/main/java/nl/thehyve/ocdu/soap/SOAPRequestFactories/CreateSubjectRequestFactory.java
+++ src/main/java/nl/thehyve/ocdu/soap/SOAPRequestFactories/CreateSubjectRequestFactory.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudyRefFactory.createStudyRef;
+import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudySubjectFactory.createStudySubject;
 
 /**
  * Created by piotrzakrzewski on 17/06/16.
@@ -23,7 +24,7 @@ import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudyRefFactory.createSt
 public class CreateSubjectRequestFactory {
 
 
-    public Collection<CreateRequest> getCreateRequests(Collection<Subject> subjects, Study study, SiteDefinition site) {
+    public static Collection<CreateRequest> getCreateRequests(Collection<Subject> subjects, Study study, SiteDefinition site) {
         List<CreateRequest> createRequests = subjects.stream().map(subject -> {
             try {
                 return getCreateRequest(subject, study, site);
@@ -35,19 +36,11 @@ public class CreateSubjectRequestFactory {
         return createRequests;
     }
 
-    private CreateRequest getCreateRequest(Subject subject, Study study, SiteDefinition site) throws DatatypeConfigurationException {
+    private static CreateRequest getCreateRequest(Subject subject, Study study, SiteDefinition site) throws DatatypeConfigurationException {
         CreateRequest request = new CreateRequest();
-        StudySubjectType subj = new StudySubjectType();
-        StudyRefType studyRef = createStudyRef(study, site);
-        subj.setStudyRef(studyRef);
-        subj.setLabel(subject.getSsid());
-        SubjectType subjType = new SubjectType();
-        XMLGregorianCalendar dateOfBirth = DatatypeFactory.newInstance().newXMLGregorianCalendar(subject.getDateOfBirth());
-        subjType.setDateOfBirth(dateOfBirth);
-        subj.setSubject(subjType);
-
-        request.setStudySubject(subj);
-        return null;
+        StudySubjectType studySubject = createStudySubject(subject, study, site);
+        request.setStudySubject(studySubject);
+        return request;
     }
 
 }
--- src/test/java/nl/thehyve/ocdu/soap/SOAPRequestFactoriesTests.java
+++ src/test/java/nl/thehyve/ocdu/soap/SOAPRequestFactoriesTests.java
@@ -1,12 +1,21 @@
 package nl.thehyve.ocdu.soap;
 
+import nl.thehyve.ocdu.models.OCEntities.Study;
 import nl.thehyve.ocdu.models.OCEntities.Subject;
+import nl.thehyve.ocdu.models.OcDefinitions.SiteDefinition;
 import org.junit.Test;
 import org.openclinica.ws.beans.GenderType;
+import org.openclinica.ws.beans.StudySubjectType;
 import org.openclinica.ws.beans.SubjectType;
+import org.openclinica.ws.studysubject.v1.CreateRequest;
 
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+
+import static nl.thehyve.ocdu.soap.SOAPRequestFactories.CreateSubjectRequestFactory.getCreateRequests;
+import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudySubjectFactory.createStudySubject;
 import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudySubjectFactory.createSubjectType;
 import static nl.thehyve.ocdu.soap.SOAPRequestFactories.StudySubjectFactory.createXMLGregorianDate;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,6 +26,33 @@ import static org.hamcrest.Matchers.*;
  */
 
 public class SOAPRequestFactoriesTests {
+
+    @Test
+    public void createSubjectRequestTest() throws Exception {
+        Subject s1 = new Subject();
+        s1.setSsid("s1");
+        Subject s2 = new Subject();
+        s2.setSsid("s2");
+        Collection<Subject> subjects = new ArrayDeque<>();
+        subjects.add(s1);
+        subjects.add(s2);
+        Study study = new Study("study1","oid","study");
+        SiteDefinition site = null;
+        Collection<CreateRequest> createRequests = getCreateRequests(subjects, study, site);
+        assertThat(createRequests, iterableWithSize(2));
+    }
+
+    @Test
+    public void createStudySubjecttest() throws Exception {
+        SiteDefinition site = null;
+        Study study = new Study("study1", "oid", "study");
+        Subject subject = new Subject();
+        subject.setSsid("subject1");
+
+        StudySubjectType studySubject = createStudySubject(subject, study, site);
+        assertThat(studySubject, is(notNullValue()));
+    }
+
     @Test
     public void createXMLGregorianDateTest() throws Exception {
         Subject subject = new Subject();
@@ -30,7 +66,7 @@ public class SOAPRequestFactoriesTests {
     public void createSubjectTypeTests_noGender_noDate() throws Exception {
         Subject subject = new Subject();
         SubjectType subjectType = createSubjectType(subject);
-        assertThat(subjectType , notNullValue());
+        assertThat(subjectType, notNullValue());
         assertThat(subjectType, hasProperty("gender", nullValue()));
         assertThat(subjectType, hasProperty("dateOfBirth", nullValue()));
     }
@@ -40,7 +76,7 @@ public class SOAPRequestFactoriesTests {
         Subject subject = new Subject();
         subject.setGender("f");
         SubjectType subjectType = createSubjectType(subject);
-        assertThat(subjectType , notNullValue());
+        assertThat(subjectType, notNullValue());
         assertThat(subjectType, hasProperty("gender", is(GenderType.F)));
         assertThat(subjectType, hasProperty("dateOfBirth", nullValue()));
     }
@@ -50,7 +86,7 @@ public class SOAPRequestFactoriesTests {
         Subject subject = new Subject();
         subject.setDateOfBirth("2000");
         SubjectType subjectType = createSubjectType(subject);
-        assertThat(subjectType , notNullValue());
+        assertThat(subjectType, notNullValue());
         assertThat(subjectType, hasProperty("gender", nullValue()));
         assertThat(subjectType, hasProperty("dateOfBirth", notNullValue(XMLGregorianCalendar.class)));
     }
@@ -61,11 +97,10 @@ public class SOAPRequestFactoriesTests {
         subject.setGender("f");
         subject.setDateOfBirth("2000");
         SubjectType subjectType = createSubjectType(subject);
-        assertThat(subjectType , notNullValue());
+        assertThat(subjectType, notNullValue());
         assertThat(subjectType, hasProperty("gender", is(GenderType.F)));
         assertThat(subjectType, hasProperty("dateOfBirth", notNullValue(XMLGregorianCalendar.class)));
     }
-
 
 
 }

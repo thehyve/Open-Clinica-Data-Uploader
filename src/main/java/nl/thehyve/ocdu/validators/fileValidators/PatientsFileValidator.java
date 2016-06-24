0e348017226ee5c7218d9f7e47f076ab8558cb84
Piotr Zakrzewski <piotr@thehyve.nl>
2016-06-24 11:26:59 +0200

    Adjust error message for banned person ID column in patients file

--- src/main/java/nl/thehyve/ocdu/validators/fileValidators/PatientsFileValidator.java
+++ src/main/java/nl/thehyve/ocdu/validators/fileValidators/PatientsFileValidator.java
@@ -1,13 +1,8 @@
 package nl.thehyve.ocdu.validators.fileValidators;
 
-import nl.thehyve.ocdu.factories.ClinicalDataFactory;
 import nl.thehyve.ocdu.factories.PatientDataFactory;
-import nl.thehyve.ocdu.models.errors.FileFormatError;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by piotrzakrzewski on 11/04/16.

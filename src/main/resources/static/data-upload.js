93bc1edd9bb144fe334a274ddac7b1155fc84940
Piotr Zakrzewski <piotr@thehyve.nl>
2016-06-27 15:57:57 +0200

    Delete failed submissions

--- src/main/resources/static/data-upload.js
+++ src/main/resources/static/data-upload.js
@@ -6,6 +6,20 @@
  */
 var sessions = [];
 
+var _deleteCurrentSubmission = function() {
+    $.ajax({
+        url: baseApp + "/submission/delete",
+        type: "post",
+        data: {},
+        success: function () {
+            console.log('submission deleted');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.status + " " + textStatus + " " + errorThrown);
+        }
+    });
+};
+
 function uploadFile() {
 
     var isSessionNameDefined = ($('#upload-session-input').val() !== "");
@@ -92,17 +106,7 @@ function uploadFile() {
                     $("#message-board").append(info);
 
                     //since this is format error, we delete the just created submission
-                    $.ajax({
-                        url: baseApp + "/submission/delete",
-                        type: "post",
-                        data: {},
-                        success: function () {
-                            console.log('submission deleted');
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            console.log(jqXHR.status + " " + textStatus + " " + errorThrown);
-                        }
-                    });
+                    _deleteCurrentSubmission();
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -111,6 +115,7 @@ function uploadFile() {
                 var info = '<div id="data-alert" class="alert alert-danger">' + message + '</div>';
                 $("#message-board").append(info);
                 console.log(jqXHR.status + " " + textStatus + " " + errorThrown);
+               _deleteCurrentSubmission();
             }
         });
     };

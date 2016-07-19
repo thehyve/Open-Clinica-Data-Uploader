413c0a9730b1bb10c987fa5f223b7ddf47b709e0
Piotr Zakrzewski <piotr@thehyve.nl>
2016-07-19 15:10:05 +0200

    Implement drop-down for environment selection

--- src/main/java/nl/thehyve/ocdu/controllers/ViewsController.java
+++ src/main/java/nl/thehyve/ocdu/controllers/ViewsController.java
@@ -1,10 +1,15 @@
 package nl.thehyve.ocdu.controllers;
 
+import nl.thehyve.ocdu.OCEnvironmentsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by piotrzakrzewski on 22/03/16.
@@ -16,8 +21,13 @@ public class ViewsController {
 
     private static final Logger log = LoggerFactory.getLogger(ViewsController.class);
 
+    @Autowired
+    OCEnvironmentsConfig ocEnvironmentsConfig;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(Model model) {
+        List<OCEnvironmentsConfig.OCEnvironment> ocEnvironments = ocEnvironmentsConfig.getOcEnvironments();
+        model.addAttribute("environments", ocEnvironments);
         return "login";
     }
 
